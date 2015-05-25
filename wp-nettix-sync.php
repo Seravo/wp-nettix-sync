4f07c86a3b65da449f23c5da812bab5f01d60fc6
Tarppa <tari@seravo.fi>
2015-05-25 14:31:04 +0300

    modified parse_meta and xml to array conversion

--- wp-nettix-sync.php
+++ wp-nettix-sync.php
@@ -108,6 +108,8 @@ function _wp_nettix_do_data_sync() {
   foreach($links as $count => $link) {
     // limit for debug
     $meta = _wp_nettix_parse_meta( $link );
+    
+    error_log(var_dump($meta,true),0);
     // make this available
     $available[] = $meta['nettixID'];
     $post = array(
@@ -139,10 +141,10 @@ function _wp_nettix_do_data_sync() {
     foreach($meta as $key => $value) {
       // store arrays in JSON
       if(is_array($value))
-        $value = json_encode( $value );
-      // add or update the value
-      if( !add_post_meta($post_id, trim( $key ), sanitize_text_field( $value ), true) )
-          update_post_meta($post_id, trim( $key ), sanitize_text_field( $value ) );
+      //$value = json_encode( $value );
+      //add or update the value
+      if( !add_post_meta($post_id, trim( $key ), sanitize_text_field($value), true) )
+          update_post_meta($post_id, trim( $key ), sanitize_text_field($value), true );
     }
   }
   // find posts to eliminate
@@ -185,9 +187,10 @@ function _wp_nettix_parse_meta($item) {
   set_time_limit(180);
   
   $xml = simplexml_load_file($item);
-  $meta = json_decode(json_encode((array)$xml),1);
+  $meta = xmlToArray($xml);
+  //$meta = json_decode(json_encode((array)$xml),1);
   // save nettix URL in meta fields
-  unset($meta[0]);
+  //unset($meta[0]);
   $meta['source'] = $item;
   // get nettix ID
   $meta['nettixID'] = (string)$xml->id;
@@ -195,15 +198,20 @@ function _wp_nettix_parse_meta($item) {
   $meta['title'] = (string)$xml->make .' '. (string)$xml->model .' '. (string)$xml->year;
   
   $images = array();
-  
   for($x=0;$x<count($meta['media']['image']);$x++){
-    $images[] = (string)$meta['media']['image'][$x]['imgUrl'];
+    $images[] = $meta['media']['image'][$x]['imgUrl'];
   }
   unset($meta['media']['image']);
   /*foreach( $meta['image'] as $element ) {
     $images[]=$element['imgUrl'];
   }*/
   $meta['images'] = $images;
+  
+  $temp = array();
+  $temp = $meta['ad'];
+  unset($meta['ad']);
+  $meta = array_merge($meta,$temp);
+  
   return $meta;
 }
 /**
@@ -229,6 +237,84 @@ function _wp_nettix_get_links(){
   }
   return $items;  
 }
+
+function xmlToArray($xml, $options = array()) {
+    $defaults = array(
+        'namespaceSeparator' => ':',//you may want this to be something other than a colon
+        'attributePrefix' => '@',   //to distinguish between attributes and nodes with the same name
+        'alwaysArray' => array(),   //array of xml tag names which should always become arrays
+        'autoArray' => true,        //only create arrays for tags which appear more than once
+        'textContent' => '$',       //key used for the text content of elements
+        'autoText' => true,         //skip textContent key if node has no attributes or child nodes
+        'keySearch' => false,       //optional search and replace on tag and attribute names
+        'keyReplace' => false       //replace values for above search values (as passed to str_replace())
+    );
+    $options = array_merge($defaults, $options);
+    $namespaces = $xml->getDocNamespaces();
+    $namespaces[''] = null; //add base (empty) namespace
+ 
+    //get attributes from all namespaces
+    $attributesArray = array();
+    foreach ($namespaces as $prefix => $namespace) {
+        foreach ($xml->attributes($namespace) as $attributeName => $attribute) {
+            //replace characters in attribute name
+            if ($options['keySearch']) $attributeName =
+                    str_replace($options['keySearch'], $options['keyReplace'], $attributeName);
+            $attributeKey = $options['attributePrefix']
+                    . ($prefix ? $prefix . $options['namespaceSeparator'] : '')
+                    . $attributeName;
+            $attributesArray[$attributeKey] = (string)$attribute;
+        }
+    }
+ 
+    //get child nodes from all namespaces
+    $tagsArray = array();
+    foreach ($namespaces as $prefix => $namespace) {
+        foreach ($xml->children($namespace) as $childXml) {
+            //recurse into child nodes
+            $childArray = xmlToArray($childXml, $options);
+            list($childTagName, $childProperties) = each($childArray);
+ 
+            //replace characters in tag name
+            if ($options['keySearch']) $childTagName =
+                    str_replace($options['keySearch'], $options['keyReplace'], $childTagName);
+            //add namespace prefix, if any
+            if ($prefix) $childTagName = $prefix . $options['namespaceSeparator'] . $childTagName;
+ 
+            if (!isset($tagsArray[$childTagName])) {
+                //only entry with this key
+                //test if tags of this type should always be arrays, no matter the element count
+                $tagsArray[$childTagName] =
+                        in_array($childTagName, $options['alwaysArray']) || !$options['autoArray']
+                        ? array($childProperties) : $childProperties;
+            } elseif (
+                is_array($tagsArray[$childTagName]) && array_keys($tagsArray[$childTagName])
+                === range(0, count($tagsArray[$childTagName]) - 1)
+            ) {
+                //key already exists and is integer indexed array
+                $tagsArray[$childTagName][] = $childProperties;
+            } else {
+                //key exists so convert to integer indexed array with previous value in position 0
+                $tagsArray[$childTagName] = array($tagsArray[$childTagName], $childProperties);
+            }
+        }
+    }
+ 
+    //get text content of node
+    $textContentArray = array();
+    $plainText = trim((string)$xml);
+    if ($plainText !== '') $textContentArray[$options['textContent']] = $plainText;
+ 
+    //stick it all together
+    $propertiesArray = !$options['autoText'] || $attributesArray || $tagsArray || ($plainText === '')
+            ? array_merge($attributesArray, $tagsArray, $textContentArray) : $plainText;
+ 
+    //return node as array
+    return array(
+        $xml->getName() => $propertiesArray
+    );
+}
+
 
 /**
  * Run sync via GET parameters
